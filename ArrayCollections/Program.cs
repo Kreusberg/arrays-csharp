e2e381b6255d3bc6e8a63ba566450cb1978fde65
Lucas Lodi Kreusberg <kreusberglucas@gmail.com>
2023-10-25 11:45:54 -0300

    create exception class ByteBankException

--- ArrayCollections/Program.cs
+++ ArrayCollections/Program.cs
@@ -1,7 +1,6 @@
-﻿using ArrayCollections.bytebank.Util;
+﻿using ArrayCollections.bytebank.Exceptions;
+using ArrayCollections.bytebank.Util;
 using bytebank.Modelos.Conta;
-using System.Collections;
-using System.Collections.Generic;
 
 Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
 
@@ -18,14 +17,14 @@ void TestaArrayInt()
     Console.WriteLine($"Tamanho do Array: {idades.Length}");
 
     int acumulador = 0;
-    for( int i = 0; i < idades.Length; i++ )
+    for (int i = 0; i < idades.Length; i++)
     {
         int idade = idades[i];
         acumulador += idade;
         Console.WriteLine($"Índice [{i}] = {idade}");
     }
     int media = acumulador / idades.Length;
-        Console.WriteLine($"Média de idades: {media}");
+    Console.WriteLine($"Média de idades: {media}");
 }
 
 void TesteBuscarPalavra()
@@ -59,7 +58,7 @@ void testaMediana(Array array)
         Console.WriteLine("Array para cálculo da mediana está vazio ou nulo.");
     }
 
-    double[] numerosOrdenados = (double[]) array.Clone();
+    double[] numerosOrdenados = (double[])array.Clone();
     Array.Sort(numerosOrdenados);
 
     int tamanho = numerosOrdenados.Length;
@@ -71,7 +70,8 @@ void testaMediana(Array array)
 
 }
 
-double TestaMedia(Array amostra) {
+double TestaMedia(Array amostra)
+{
 
     if ((amostra == null) || (amostra.Length == 0))
     {
@@ -83,7 +83,7 @@ double TestaMedia(Array amostra) {
         double somaTotal = 0;
         for (int i = 0; i < amostra.Length; i++)
         {
-            somaTotal += (double) amostra.GetValue(i)!;
+            somaTotal += (double)amostra.GetValue(i)!;
         }
 
         double media = somaTotal / amostra.Length;
@@ -135,36 +135,94 @@ List<ContaCorrente> _listaDeContas = new List<ContaCorrente>() {
     new ContaCorrente(94, "987321-W") {Saldo=60}
 };
 
+#region Exemplos de Uso Do List
+//List<ContaCorrente> _listaDeContas2 = new List<ContaCorrente>()
+//{
+//    new ContaCorrente(874, "5679787-A"),
+//    new ContaCorrente(874, "4456668-B"),
+//    new ContaCorrente(874, "7781438-C")
+//};
+
+//List<ContaCorrente> _listaDeContas3 = new List<ContaCorrente>()
+//{
+//    new ContaCorrente(951, "5679787-E"),
+//    new ContaCorrente(321, "4456668-F"),
+//    new ContaCorrente(719, "7781438-G")
+//};
+
+//_listaDeContas2.AddRange(_listaDeContas3);
+//_listaDeContas2.Reverse();
+
+//for (int i = 0; i < _listaDeContas2.Count; i++)
+//{
+//    Console.WriteLine($"Indice[{i}] = Conta [{_listaDeContas2[i].Conta}]");
+//}
+
+//Console.WriteLine("\n\n");
+
+//var range = _listaDeContas3.GetRange(0, 1);
+
+//for (int i = 0; i < range.Count; i++)
+//{
+//    Console.WriteLine($"Indice[{i}] = Conta [{range[i].Conta}]");
+
+//    Console.WriteLine("\n\n");
+
+//}
+
+//_listaDeContas3.Clear();
+//for (int i = 0; i < _listaDeContas3.Count; i++)
+//{
+//    Console.WriteLine($"Indice[{i}] = Conta [{_listaDeContas3[i].Conta}]");
+//}
+#endregion
+
 void AtendimentoCliente()
 {
-    char opcao = '0';
-    while (opcao!= '6')
+    try
     {
-        Console.Clear();
-        Console.WriteLine("================================");
-        Console.WriteLine("===       Atendimento        ===");
-        Console.WriteLine("=== 1 - Cadastrar Conta      ===");
-        Console.WriteLine("=== 2 - Listar Contas        ===");
-        Console.WriteLine("=== 3 - Remover Conta        ===");
-        Console.WriteLine("=== 4 - Ordenar Contas       ===");
-        Console.WriteLine("=== 5 - Pesquisar Conta      ===");
-        Console.WriteLine("=== 6 - Sair do Sistema      ===");
-        Console.WriteLine("================================");
-        Console.WriteLine("\n\n");
-        Console.Write("Digite a opção desejada: ");
-        opcao = Console.ReadLine()[0];
-        switch (opcao)
+        char opcao = '0';
+        while (opcao != '6')
         {
-            case '1':
-                CadastrarConta();
-                break;
-            case '2':
-                ListarConta();
-                break;
-            default:
-                Console.WriteLine("Opcao não implementada.");
-                break;
+            Console.Clear();
+            Console.WriteLine("================================");
+            Console.WriteLine("===       Atendimento        ===");
+            Console.WriteLine("=== 1 - Cadastrar Conta      ===");
+            Console.WriteLine("=== 2 - Listar Contas        ===");
+            Console.WriteLine("=== 3 - Remover Conta        ===");
+            Console.WriteLine("=== 4 - Ordenar Contas       ===");
+            Console.WriteLine("=== 5 - Pesquisar Conta      ===");
+            Console.WriteLine("=== 6 - Sair do Sistema      ===");
+            Console.WriteLine("================================");
+            Console.WriteLine("\n\n");
+            Console.Write("Digite a opção desejada: ");
+            try
+            {
+                opcao = Console.ReadLine()[0];
+
+            }
+            catch (Exception excecao)
+            {
+
+                throw new ByteBankException(excecao.Message);
+            }
+            switch (opcao)
+            {
+                case '1':
+                    CadastrarConta();
+                    break;
+                case '2':
+                    ListarConta();
+                    break;
+                default:
+                    Console.WriteLine("Opcao não implementada.");
+                    break;
+            }
         }
+    }
+    catch (ByteBankException excecao)
+    {
+        Console.WriteLine($"{excecao.Message}");
     }
 }
 
@@ -230,18 +288,4 @@ void ListarConta()
     }
 }
 
-//AtendimentoCliente();
-
-//Generica<int> teste1 = new Generica<int>();
-//teste1.MostrarMensagem(10);
-
-//Generica<string> teste2 = new Generica<string>();
-//teste2.MostrarMensagem("Olá mundo!");
-
-//public class Generica<T>
-//{
-//    public void MostrarMensagem(T t)
-//    {
-//        Console.WriteLine($"Exibindo {t}");
-//    }
-//}
+AtendimentoCliente();
