d78c6ca5508cea357e318b7d072b46d1c552f0df
Lucas Lodi Kreusberg <kreusberglucas@gmail.com>
2023-10-26 16:03:44 -0300

    create ExibirListarDeContas method

--- ArrayCollections/Program.cs
+++ ArrayCollections/Program.cs
@@ -1,6 +1,7 @@
 ﻿using ArrayCollections.bytebank.Exceptions;
 using ArrayCollections.bytebank.Util;
 using bytebank.Modelos.Conta;
+using System.ComponentModel.Design;
 
 Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
 
@@ -340,13 +341,13 @@ void PesquisarConta()
     Console.WriteLine("===    PESQUISAR CONTAS     ===");
     Console.WriteLine("===============================");
     Console.WriteLine("\n");
-    Console.Write("Deseja pesquisar por (1) NUMERO DA CONTA ou (2) CPF TITULAR ? ");
+    Console.Write("Deseja pesquisar por (1) NÚMERO DA CONTA , (2) CPF TITULAR  ou (3) NÚMERO DA AGÊNCIA ? ");
     switch (int.Parse(Console.ReadLine()))
     {
         case 1:
             {
                 Console.Write("Informe o número da Conta: ");
-                string _numeroConta = Console.ReadLine()!;
+                string _numeroConta = Console.ReadLine();
                 ContaCorrente consultaConta = ConsultaPorNumeroConta(_numeroConta);
                 Console.WriteLine(consultaConta.ToString());
                 Console.ReadKey();
@@ -355,17 +356,28 @@ void PesquisarConta()
         case 2:
             {
                 Console.Write("Informe o CPF do Titular: ");
-                string _cpf = Console.ReadLine()!;
+                string _cpf = Console.ReadLine();
                 ContaCorrente consultaCpf = ConsultaPorCPFTitular(_cpf);
                 Console.WriteLine(consultaCpf.ToString());
                 Console.ReadKey();
                 break;
             }
+        case 3:
+            {
+                Console.Write("Informe o N° da Agência: ");
+                int _numeroAgencia = int.Parse(Console.ReadLine());
+                var contasPorAgencia = ConsultaPorAgencia(_numeroAgencia);
+                ExibirListarDeContas(contasPorAgencia);
+                Console.ReadKey();
+                break;
+            }
+
         default:
             Console.WriteLine("Opção não implementada.");
             break;
     }
 }
+
 
 ContaCorrente ConsultaPorCPFTitular(string? cpf)
 {
@@ -378,7 +390,7 @@ ContaCorrente ConsultaPorCPFTitular(string? cpf)
     //    }
     //}
     //return conta;
-    return _listaDeContas.Where(conta => conta.Titular.Cpf.Equals(cpf)).FirstOrDefault()!;
+    return _listaDeContas.Where(conta => conta.Titular.Cpf.Equals(cpf)).FirstOrDefault();
 }
 
 ContaCorrente ConsultaPorNumeroConta(string? numeroConta)
@@ -393,6 +405,32 @@ ContaCorrente ConsultaPorNumeroConta(string? numeroConta)
     //}
     //return conta;
     return _listaDeContas.Where(conta => conta.Conta.Equals(numeroConta)).FirstOrDefault();
+}
+
+List<ContaCorrente> ConsultaPorAgencia(int numeroAgencia)
+{
+    var consulta = (
+                         from conta in _listaDeContas
+                         where conta.Numero_agencia == numeroAgencia
+                         select conta).ToList();
+    return consulta;
+}
+
+void ExibirListarDeContas(List<ContaCorrente> contasPorAgencia)
+{
+    if (contasPorAgencia == null)
+    {
+        Console.WriteLine("... A consulta não retornou dados ...");
+    }
+    else
+    {
+        foreach (var item in contasPorAgencia)
+        {
+            Console.WriteLine(item.ToString());
+
+        }
+    }
+
 }
 
 AtendimentoCliente();
